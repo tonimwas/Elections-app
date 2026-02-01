8eae6480db0648f4ed9e1bff42a865204a773972
tonimwas <mikeanto098@gmail.com>
2026-02-01 20:18:03 +0300

    added some server optimisation

--- electionsapp/views.py
+++ electionsapp/views.py
@@ -1,19 +1,38 @@
+import json
+from django.contrib.gis.db.models.functions import AsGeoJSON
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Constituency
-from .serializers import ConstituencySerializer
 
 
+@method_decorator(cache_page(60 * 5), name='dispatch')
 class ConstituencyGeoJSONView(APIView):
     """Return all constituencies as a GeoJSON FeatureCollection."""
 
     def get(self, request):
-        queryset = Constituency.objects.all().order_by('name')
-        serializer = ConstituencySerializer(queryset, many=True)
+        queryset = (
+            Constituency.objects.all()
+            .order_by('name')
+            .annotate(geometry=AsGeoJSON('geom'))
+            .values(
+                'id',
+                'name',
+                'updated_name',
+                'mp',
+                'party',
+                'impeachment_vote',
+                'budget_vote',
+                'county',
+                'registered_voters',
+                'geometry',
+            )
+        )
 
         features = []
-        for entry in serializer.data:
+        for entry in queryset:
             entry_data = dict(entry)
             geometry = entry_data.pop('geometry', None)
             features.append(
@@ -21,11 +40,13 @@ class ConstituencyGeoJSONView(APIView):
                     'type': 'Feature',
                     'id': entry_data['id'],
                     'properties': entry_data,
-                    'geometry': geometry,
+                    'geometry': json.loads(geometry) if geometry else None,
                 }
             )
 
-        return Response({
-            'type': 'FeatureCollection',
-            'features': features,
-        })
+        return Response(
+            {
+                'type': 'FeatureCollection',
+                'features': features,
+            }
+        )
